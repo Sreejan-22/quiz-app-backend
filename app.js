6c8b7d7b304fb4764840f6e09af8b05f3e68fd28
Sreejan <sreejan147@gmail.com>
2021-10-31 20:23:36 +0530

    feat: setup app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+// handle cors
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+const port = process.env.PORT;
+const dbURI = process.env.MONGO_URI;
+
+// connect db
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
+// routes
+
+// 404
+app.use("/", (req, res) => {
+  res.status(404).json({ success: false, message: "404 page!! Not found!!" });
+});
