b2e4549fa256580521ac98c09145cd4265e1eb36
Sreejan <sreejan147@gmail.com>
2021-11-01 14:21:34 +0530

    feat: add auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const { createToken } = require("../utils/createToken");
+const { handleError } = require("../utils/handleSignupErrors");
+
+module.exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.create({ name, email, password });
+
+    const saltRounds = 10;
+    // auto generate a salt and hash
+    user.password = await bcrypt.hash(user.password, saltRounds);
+    const newUser = await user.save();
+
+    // after a new account is created, the user is logged into the website
+    // so a token, to be sent to the frontend, has to be generated
+    const token = createToken(newUser._id);
+    res.status(201).json({ success: true, user: newUser, token });
+  } catch (err) {
+    const errors = handleError(err);
+
+    if (errors.name === "" && errors.email === "" && errors.password === "") {
+      res.status(500).json({
+        success: false,
+        serverError: true,
+        message: "Oops!! Something went wrong!",
+        error: err,
+      });
+    } else {
+      res.status(400).json({ success: false, errors });
+    }
+  }
+};
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        const token = createToken(user._id);
+        res.status(200).json({ success: true, user, token });
+      } else {
+        res.status(401).json({ success: false, message: "Incorrect Password" });
+      }
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "This email is not registered" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      serverError: true,
+      message: "Oops!! Something went wrong!",
+      error: err,
+    });
+  }
+};
