ad49a0b3b834b341d8ba904287056e3ea0c2100e
Sreejan <sreejan147@gmail.com>
2021-11-01 15:40:31 +0530

    feat: add question model

---
+++ models/question.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const questionModel = new Schema({
+  // 3 categories - sports, politics, history
+  category: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: [String],
+    required: true,
+  },
+});
+
+const Question = mongoose.model("question", questionModel);
+module.exports = Question;
