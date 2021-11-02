0b93d49490c4facc74a2bb08df54fcc1c74d1e1c
Sreejan <sreejan147@gmail.com>
2021-11-02 20:07:02 +0530

    feat: add auth middleware to a quiz route

--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -3,10 +3,11 @@ const {
   getQuestionsOfCategory,
   insertData,
 } = require("../controllers/quiz.controller");
+const { checkAuthentication } = require("../middlewares/auth.middleware");
 
 const router = Router();
 
-router.get("/quiz/:category", getQuestionsOfCategory);
+router.get("/quiz/:category", checkAuthentication, getQuestionsOfCategory);
 router.post("/quiz/:category", insertData);
 
 module.exports = router;
