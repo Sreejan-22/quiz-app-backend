3623128e3d92434677b25350a66956b986192594
Sreejan <sreejan147@gmail.com>
2021-11-07 21:49:14 +0530

    feat: modified quiz controller

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -17,33 +17,24 @@ const getQuestionsOfCategory = async (req, res) => {
 };
 
 const insertData = async (req, res) => {
-  // fs.readFile("./data/sports.json", "utf-8", (err, jsonString) => {
-  //   if (err) {
-  //     console.log("File read failed: ", err);
-  //     return;
-  //   } else {
-  //     try {
-  //       const jsonData = JSON.parse(jsonString);
-  //       const questions = jsonData.questions;
-  //       Question.insertMany(questions, function (err, result) {
-  //         if (err) {
-  //           res.send(err);
-  //         } else {
-  //           res.send(result);
-  //         }
-  //       });
-  //       console.log(createdData);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   }
-  // });
   try {
-    const { category } = req.params;
-    const jsonString = await fs.readFile(`./data/${category}.json`, "utf-8");
-    const jsonData = JSON.parse(jsonString);
-    const questions = jsonData.questions;
-    const insertedData = await Question.insertMany(questions);
+    const sports = await fs.readFile(`./data/sports.json`, "utf-8");
+    const history = await fs.readFile(`./data/history.json`, "utf-8");
+    const politics = await fs.readFile(`./data/politics.json`, "utf-8");
+
+    const sportsQuestions = JSON.parse(sports).questions;
+    const historyQuestions = JSON.parse(history).questions;
+    const politicsQuestions = JSON.parse(politics).questions;
+
+    const questions = [
+      ...sportsQuestions,
+      ...historyQuestions,
+      ...politicsQuestions,
+    ];
+
+    console.log(questions);
+
+    const insertedData = await Question.insertMany(questions, { limit: 30 });
     res.status(201).json({ success: true, message: "Questions stored in DB" });
   } catch (err) {
     res.status(400).json({
@@ -54,7 +45,22 @@ const insertData = async (req, res) => {
   }
 };
 
+const deleteAll = async (req, res) => {
+  try {
+    const temp = await Question.deleteMany({});
+    res.status(200).json({ success: true, message: "All data deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      success: false,
+      message: "Failed to delete data",
+      error: err,
+    });
+  }
+};
+
 module.exports = {
   getQuestionsOfCategory,
   insertData,
+  deleteAll,
 };
--- routes/quiz.route.js
+++ routes/quiz.route.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 const {
   getQuestionsOfCategory,
   insertData,
+  deleteAll,
 } = require("../controllers/quiz.controller");
 const { checkAuthentication } = require("../middlewares/auth.middleware");
 
 const router = Router();
 
 router.get("/quiz/:category", checkAuthentication, getQuestionsOfCategory);
-router.post("/quiz/:category", insertData);
+router.post("/quiz", insertData);
+router.delete("/quiz", deleteAll);
 
 module.exports = router;
